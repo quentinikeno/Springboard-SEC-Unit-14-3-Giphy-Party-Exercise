571e8c7e4b56fa20a649671ef3f73520df246d24
Quentin Ikeno <ikenoq@gmail.com>
2022-01-22 10:51:08 -1000

    Add styles, remove button, and refactor

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
+const $gifRow = $("#gifRow");
+const search = document.querySelector("#search");
+const form = document.querySelector("#searchForm");
+const $removeBtn = $("#removeButton");
+
 async function getGif(q) {
-	const api_key = "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym";
-	const params = { api_key, q };
-	const res = await axios.get("http://api.giphy.com/v1/gifs/search", {
-		params,
-	});
-	const result = res.data.data[0];
-	const gifRow = $("#gifRow");
+	try {
+		const api_key = "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym";
+		const params = { api_key, q };
+		const res = await axios.get("http://api.giphy.com/v1/gifs/search", {
+			params,
+		});
+		const randIndex = Math.floor(Math.random() * res.data.data.length);
+		const result = res.data.data[randIndex];
+		appendGif(result);
+	} catch (e) {
+		alert("Something has gone wrong!  GIF not found!");
+	}
+}
+
+function appendGif(result) {
 	const imgDiv = $("<div>").addClass(
 		"col-12 col-md-6 col-lg-4 mt-3 d-flex align-items-center justify-content-center"
 	);
@@ -13,14 +26,18 @@ async function getGif(q) {
 	const img = $("<img>")
 		.attr("src", imgUrl)
 		.attr("alt", result.title)
-		.addClass("img-fluid");
-	gifRow.append(imgDiv.append(img));
+		.addClass("img-fluid gif");
+	$gifRow.append(imgDiv.append(img).hide().fadeIn(1000));
 }
 
-const search = document.querySelector("#search");
-const form = document.querySelector("#searchForm");
-form.addEventListener("submit", (event) => {
+form.addEventListener("submit", async (event) => {
 	event.preventDefault();
 	getGif(search.value);
 	search.value = "";
+});
+
+$removeBtn.on("click", () => {
+	$gifRow.children().fadeOut(1000, function () {
+		$(this).remove();
+	});
 });
